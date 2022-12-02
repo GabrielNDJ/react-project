3878f482a9f20e201da07783a63714ff6011f71d
GabrielNDJ <gabrielnietodejesus@gmail.com>
2022-12-02 20:35:23 -0300

    Reparacion de ClassName

--- src/components/Inicio.js
+++ src/components/Inicio.js
@@ -16,11 +16,11 @@ const Inicio = () => {
             <div className="row">
                 {products.map(product => (
                     <div className="col-md-3" key={product.id}>
-                        <div class="card" style={{ width: '18rem' }}>
-                            <img class="card-img-top" src={product.image} alt={product.title} />
-                            <div class="card-body">
-                                <h5 class="card-title">{product.title}</h5>
-                                <p class="card-text">{product.description}</p>
+                        <div className="card" style={{ width: '18rem' }}>
+                            <img className="card-img-top" src={product.image} alt={product.title} />
+                            <div className="card-body">
+                                <h5 className="card-title">{product.title}</h5>
+                                <p className="card-text">{product.description}</p>
                             </div>
                         </div>
                     </div>
